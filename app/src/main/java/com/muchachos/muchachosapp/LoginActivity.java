1123dec5de36308e88a95ced017215e7d94a2043
Jhonny Morocho <jhonnymichaeldj2011@hotmail.com>
2018-03-15 12:49:29 -0500

    Interfaz

--- app/src/main/java/com/muchachos/muchachosapp/LoginActivity.java
+++ app/src/main/java/com/muchachos/muchachosapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.muchachos.muchachosapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -29,6 +30,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.muchachos.muchachosapp.Vista.RegistroActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +42,11 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
+
+    Button buttonEntrar;
     /**
+     *
+     *
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
@@ -67,6 +74,20 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        buttonEntrar=(Button)findViewById(R.id.email_sign_in_button);
+
+        buttonEntrar.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(), RegistroActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
